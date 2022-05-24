b4845ddc64f446f537d9abaad5c59b79aa0957da
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-24 12:10:43 +0300

    Protecting Frontend Pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
+        {/* we grant access to AuthPage page only if a user is not authenticated  */}
+        {!authCtx.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        {/* we render UserProfile compenent only if user is authenticated */}
+        <Route path="/profile">
+          {authCtx.isLoggedIn && <UserProfile />}
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+
+        {/* anything the user entered, if it has not been handled by one of the other routes,
+        and here we could then render a 404 page or what I will do here, we redirect the user.
+        And that will kick in whenever the user enters something invalid or if the user enters /profile without being logged in. */}
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
