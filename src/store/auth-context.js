87bc25b260e0857d86d819f44fcc43219f147e9a
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-24 16:37:27 +0300

    Finishing Steps

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
   token: "",
@@ -10,7 +12,7 @@ const AuthContext = React.createContext({
 const calculateRemainingTime = (expirationTime) => {
   // getting current timestamp in milliseconds
   const currentTime = new Date().getTime();
-  
+
   // passing expiration time to new date to convert it to a date object
   const adjExpirationTime = new Date(expirationTime).getTime();
 
@@ -19,12 +21,36 @@ const calculateRemainingTime = (expirationTime) => {
   return remainingDuration;
 };
 
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationDate = localStorage.getItem("expirationTime");
+
+  const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+  if (remainingTime <= 60000) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+};
+
 export const AuthContextProvider = (props) => {
+  const tokenData = retrieveStoredToken();
+  let initialToken;
+  if (tokenData) {
+    initialToken = tokenData.token;
+  }
+
   // we don't need useEffect here becase localStorage is synchronous API
   // when page is being refreshed we check whether our token is located in local storage or not
   // we can just set the initial token value by looking into local storage. And initial value will only be used once by React when this state is first initialized.
   // So if that runs thereafter we won't overwrite any state changes with that token.
-  const initialToken = localStorage.getItem("token");
+
   const [token, setToken] = useState(initialToken);
 
   //  !!token might look strange, but this simply converts this truthy or falsy value to a true or false Boolean value.
@@ -32,20 +58,33 @@ export const AuthContextProvider = (props) => {
   // That's a default JavaScript trick you could say.
   const userIsLoggedIn = !!token;
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
     localStorage.removeItem("token");
-  };
+    localStorage.removeItem("expirationTime");
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     // we save token in local storage to keep a user logged in after a page refresh
     localStorage.setItem("token", token);
+    // expirationTime should be a string
+    localStorage.setItem("expirationTime", expirationTime);
 
     const remainingTime = calculateRemainingTime(expirationTime);
-
-    setTimeout(logoutHandler, remainingTime)
+    // store identifier on logoutTimer
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+
+  useEffect(() => {
+    if (tokenData) {
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  }, [tokenData, logoutHandler]);
 
   const contextValue = {
     token: token,
